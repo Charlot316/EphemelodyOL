18ea1976b50cf330ab4626024bc6e770232b0184
Charlot316 <2280583075@qq.com>
2022-05-10 00:39:52 +0800

    大修改Track的算法

--- src/components/PlayInterface/Track.vue
+++ src/components/PlayInterface/Track.vue
@@ -69,7 +69,13 @@
                   Track.tempB +
                   ',0.8) 100%)',
               ]
-            : 'rgba(' + Track.tempR + ',' + Track.tempG + ',' + Track.tempB + ',0.8)',
+            : 'rgba(' +
+              Track.tempR +
+              ',' +
+              Track.tempG +
+              ',' +
+              Track.tempB +
+              ',0.8)',
         ],
       }"
     >
@@ -84,62 +90,16 @@
           :Global="Global"
         />
       </div>
-
-      <div v-for="MoveOperation in Track.moveOperations" :key="MoveOperation">
-        <MoveOperation
-          :Operation="MoveOperation"
-          v-if="
-            Global.currentTime > MoveOperation.startTime &&
-              Global.currentTime < MoveOperation.endTime + 2*refreshTime
-          "
-          :Global="Global"
-          :Track="Track"
-        />
-      </div>
-      <div
-        v-for="WidthOperation in Track.changeWidthOperations"
-        :key="WidthOperation"
-      >
-        <WidthOperation
-          :Operation="WidthOperation"
-          v-if="
-            Global.currentTime > WidthOperation.startTime &&
-              Global.currentTime < WidthOperation.endTime + 2*refreshTime
-          "
-          :Global="Global"
-          :Track="Track"
-        />
-      </div>
-      <div
-        v-for="ColorOperation in Track.changeColorOperations"
-        :key="ColorOperation"
-      >
-        <ColorOperation
-          :Operation="ColorOperation"
-          v-if="
-            Global.currentTime > ColorOperation.startTime &&
-              Global.currentTime < ColorOperation.endTime + 2*refreshTime
-          "
-          :Global="Global"
-          :Track="Track"
-        />
-      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Note from "./Note";
-import MoveOperation from "./MoveOperation";
-import WidthOperation from "./WidthOperation";
-import ColorOperation from "./ColorOperation";
 export default {
   props: ["Track", "Global"],
   components: {
     Note,
-    MoveOperation,
-    WidthOperation,
-    ColorOperation,
   },
   data() {
     return {
@@ -147,24 +107,25 @@ export default {
       isActive: false,
       lengthForBlackPoint: 15,
       refreshTime: 1000 / this.$store.state.refreshRate,
-      widthPath:[],
-      positionXPath:[],
-      RPath:[],
-      GPath:[],
-      BPath:[],
+      widthPath: [],
+      positionXPath: [],
+      RGBPath: [],
+      widthIndex: 0,
+      positionXIndex: 0,
+      RGBIndex: 0,
     };
   },
   watch: {
-    "Global.screenWidth"() {
-      
+    "Global.currentTime"() {
+      this.myTrack.tempPositionX = this.getPositionX();
+      this.myTrack.tempWidth = this.getWidth();
+      this.myTrack.tempR = this.getRGB()[0];
+      this.myTrack.tempG = this.getRGB()[1];
+      this.myTrack.tempB = this.getRGB()[2];
     },
   },
   created() {
-    this.myTrack.tempR=this.Track.R;
-    this.myTrack.tempG=this.Track.G;
-    this.myTrack.tempB=this.Track.B;
-    this.myTrack.tempWidth=this.Track.width;
-    this.myTrack.tempPositionX=this.Track.positionX;
+    this.initiate();
   },
   computed: {
     height() {
@@ -184,6 +145,305 @@ export default {
         Math.sqrt(this.lengthForBlackPoint * this.lengthForBlackPoint * 2) -
         this.lengthForBlackPoint
       );
+    },
+  },
+  methods: {
+    initiate() {
+      this.myTrack.tempPositionX = this.Track.positionX;
+      this.myTrack.tempWidth = this.Track.width;
+      this.myTrack.tempR = this.Track.R;
+      this.myTrack.tempG = this.Track.G;
+      this.myTrack.tempB = this.Track.B;
+      this.generateWidthPath();
+      this.generatePositionXPath();
+      this.generateRGBPath();
+    },
+    generateWidthPath() {
+      let length = this.myTrack.changeWidthOperations.length;
+      let start = this.Track.startTiming;
+      let end = start;
+      if (length == 0) {
+        end = this.Track.endTiming;
+      } else {
+        this.myTrack.changeWidthOperations.sort(function(a, b) {
+          return a.startTime - b.startTime;
+        });
+        end = this.myTrack.changeWidthOperations[0].startTime;
+      }
+      this.widthPath.push({
+        type: 0,
+        width: this.myTrack.width,
+        startTime: start,
+        endTime: end,
+      });
+      for (let i = 0; i < length; i++) {
+        let operation = this.myTrack.changeWidthOperations[i];
+        start = operation.startTime;
+        end = operation.endTime;
+        if (operation.startTime != operation.endTime) {
+          let k =
+            (operation.endWidth - operation.startWidth) /
+            (operation.endTime - operation.startTime);
+          let b = operation.endWidth - k * operation.endTime;
+          this.widthPath.push({
+            type: 1,
+            k: k,
+            b: b,
+            startTime: start,
+            endTime: end,
+          });
+        }
+        if (i != length - 1) {
+          let nextOperation = this.myTrack.changeWidthOperations[i + 1];
+          if (end < nextOperation.startTime) {
+            this.widthPath.push({
+              type: 0,
+              width: operation.endWidth,
+              startTime: end,
+              endTime: nextOperation.startTime,
+            });
+          }
+        } else {
+          if (end < this.Track.endTiming) {
+            this.widthPath.push({
+              type: 0,
+              width: operation.endWidth,
+              startTime: end,
+              endTime: this.Track.endTiming,
+            });
+          }
+        }
+      }
+    },
+    generatePositionXPath() {
+      let length = this.myTrack.moveOperations.length;
+      let start = this.Track.startTiming;
+      let end = start;
+      if (length == 0) {
+        end = this.Track.endTiming;
+      } else {
+        this.myTrack.moveOperations.sort(function(a, b) {
+          return a.startTime - b.startTime;
+        });
+        end = this.myTrack.moveOperations[0].startTime;
+      }
+      this.positionXPath.push({
+        type: 0,
+        positionX: this.myTrack.positionX,
+        startTime: start,
+        endTime: end,
+      });
+      for (let i = 0; i < length; i++) {
+        let operation = this.myTrack.moveOperations[i];
+        start = operation.startTime;
+        end = operation.endTime;
+        if (operation.startTime != operation.endTime) {
+          let k =
+            (operation.endX - operation.startX) /
+            (operation.endTime - operation.startTime);
+          let b = operation.endX - k * operation.endTime;
+          this.positionXPath.push({
+            type: 1,
+            k: k,
+            b: b,
+            startTime: start,
+            endTime: end,
+          });
+        }
+        if (i != length - 1) {
+          let nextOperation = this.myTrack.moveOperations[i + 1];
+          if (end < nextOperation.startTime) {
+            this.positionXPath.push({
+              type: 0,
+              positionX: operation.endX,
+              startTime: end,
+              endTime: nextOperation.startTime,
+            });
+          }
+        } else {
+          if (end < this.Track.endTiming) {
+            this.positionXPath.push({
+              type: 0,
+              positionX: operation.endX,
+              startTime: end,
+              endTime: this.Track.endTiming,
+            });
+          }
+        }
+      }
+    },
+    generateRGBPath() {
+      let length = this.myTrack.changeColorOperations.length;
+      let start = this.Track.startTiming;
+      let end = start;
+      if (length == 0) {
+        end = this.Track.endTiming;
+      } else {
+        this.myTrack.changeColorOperations.sort(function(a, b) {
+          return a.startTime - b.startTime;
+        });
+        end = this.myTrack.changeColorOperations[0].startTime;
+      }
+      this.RGBPath.push({
+        type: 0,
+        R: this.myTrack.R,
+        G: this.myTrack.G,
+        B: this.myTrack.B,
+        startTime: start,
+        endTime: end,
+      });
+      for (let i = 0; i < length; i++) {
+        let operation = this.myTrack.changeColorOperations[i];
+        start = operation.startTime;
+        end = operation.endTime;
+        if (operation.startTime != operation.endTime) {
+          let Rk =
+            (operation.endR - operation.startR) /
+            (operation.endTime - operation.startTime);
+          let Rb = operation.endR - Rk * operation.endTime;
+          let Gk =
+            (operation.endG - operation.startG) /
+            (operation.endTime - operation.startTime);
+          let Gb = operation.endG - Gk * operation.endTime;
+          let Bk =
+            (operation.endB - operation.startB) /
+            (operation.endTime - operation.startTime);
+          let Bb = operation.endB - Bk * operation.endTime;
+          this.RGBPath.push({
+            type: 1,
+            Rk: Rk,
+            Rb: Rb,
+            Gk: Gk,
+            Gb: Gb,
+            Bk: Bk,
+            Bb: Bb,
+            startTime: start,
+            endTime: end,
+          });
+        }
+        if (i != length - 1) {
+          let nextOperation = this.myTrack.changeColorOperations[i + 1];
+          if (end < nextOperation.startTime) {
+            this.RGBPath.push({
+              type: 0,
+              R: operation.endR,
+              G: operation.endG,
+              B: operation.endB,
+              startTime: end,
+              endTime: nextOperation.startTime,
+            });
+          }
+        } else {
+          if (end < this.Track.endTiming) {
+            this.RGBPath.push({
+              type: 0,
+              R: operation.endR,
+              G: operation.endG,
+              B: operation.endB,
+              startTime: end,
+              endTime: this.Track.endTiming,
+            });
+          }
+        }
+      }
+    },
+    binaryGetCurrentIndex(currentTime, path) {
+      let right = path.length - 1;
+      if (right == 0) {
+        return 0;
+      }
+      let left = 0;
+      let mid = Math.floor((right + left) / 2);
+      while (right > left) {
+        if (
+          path[mid].startTime <= currentTime &&
+          path[mid].endTime >= currentTime
+        ) {
+          return mid;
+        }
+        if (path[mid].startTime > currentTime) {
+          right = mid - 1;
+        }
+        if (path[mid].endTime < currentTime) {
+          left = mid + 1;
+        }
+        mid = Math.floor((right + left) / 2);
+      }
+      return mid;
+    },
+    getPositionX() {
+      var currentTime = this.Global.currentTime;
+      var currentX = this.positionXPath[this.positionXIndex];
+      if (
+        !(currentTime <= currentX.endTime && currentTime >= currentX.startTime)
+      ) {
+        this.positionXIndex = this.binaryGetCurrentIndex(
+          currentTime,
+          this.positionXPath
+        );
+        currentX = this.positionXPath[this.positionXIndex];
+      }
+      if (currentX.type == 0) {
+        return currentX.positionX;
+      }
+      if (currentX.type == 1) {
+        return currentX.k * currentTime + currentX.b;
+      }
+    },
+    getWidth() {
+      var currentTime = this.Global.currentTime;
+      var currentWidth = this.widthPath[this.widthIndex];
+      if (
+        !(
+          currentTime <= currentWidth.endTime &&
+          currentTime >= currentWidth.startTime
+        )
+      ) {
+        this.widthIndex = this.binaryGetCurrentIndex(
+          currentTime,
+          this.widthPath
+        );
+        currentWidth = this.widthPath[this.widthIndex];
+      }
+      if (currentWidth.type == 0) {
+        return currentWidth.width;
+      }
+      if (currentWidth.type == 1) {
+        return currentWidth.k * currentTime + currentWidth.b;
+      }
+    },
+    getRGB() {
+      var currentTime = this.Global.currentTime;
+      var currentRGB = this.RGBPath[this.RGBIndex];
+      if (
+        !(
+          currentTime <= currentRGB.endTime &&
+          currentTime >= currentRGB.startTime
+        )
+      ) {
+        this.RGBIndex = this.binaryGetCurrentIndex(currentTime, this.RGBPath);
+        currentRGB = this.RGBPath[this.RGBIndex];
+      }
+      if (currentRGB.type == 0) {
+        return [currentRGB.R, currentRGB.G, currentRGB.B];
+      }
+      if (currentRGB.type == 1) {
+        return [
+          currentRGB.Rk * currentTime + currentRGB.Rb,
+          currentRGB.Gk * currentTime + currentRGB.Gb,
+          currentRGB.Bk * currentTime + currentRGB.Bb,
+        ];
+      }
+    },
+    returnResult(path, currentTime) {
+      let index = this.binaryGetCurrentIndex(currentTime, path);
+      if (path[index].type == 0) {
+        return path[index].value;
+      } else {
+        let k = path[index].k;
+        let b = path[index].b;
+        return k * currentTime + b;
+      }
     },
   },
 };
