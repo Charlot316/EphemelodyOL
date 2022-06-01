08615ff6a9c38d37afb3026b05eb4c85f0951d0a
Charlot316 <2280583075@qq.com>
2022-06-01 19:21:38 +0800

    基本布局

--- src/components/ChartMaker/TrackCardPanel.vue
+++ src/components/ChartMaker/TrackCardPanel.vue
@@ -1,13 +1,19 @@
 <template>
-  <div :class="track.edit?'panel-edit':'panel-no-edit'">
-    <div style="background:rgb(255,255,255);height:80px;"></div>
+  <div :class="track.edit ? 'panel-edit' : 'panel-no-edit'">
+    <div class="track-tracks">
+      <div :class="track.edit ? 'note-track-edit' : 'note-track'"></div>
+      <div v-if="track.edit">
+        <div class="positionX-track"></div>
+        <div class="width-track"></div>
+        <div class="color-track"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-props: ["track", "global", "chart"],
-
+  props: ["track", "global", "chart"],
 };
 </script>
 
@@ -18,6 +24,7 @@ props: ["track", "global", "chart"],
   padding-bottom: 5px;
   border-radius: 5px;
   transition: 0.5s;
+  position:relative;
 }
 .panel-edit {
   height: 515px;
@@ -25,5 +32,53 @@ props: ["track", "global", "chart"],
   padding-bottom: 5px;
   border-radius: 5px;
   transition: 0.5s;
+  position:relative;
+}
+.track-tracks{
+  position:absolute;
+  top:0;
+  left:0;
+}
+.note-track {
+  background: rgb(255, 255, 255);
+  position:absolute;
+  top:5px;
+  left:0px;
+  height: 80px;
+  transition: 0.5s;
+  width:10000px;
+}
+.note-track-edit {
+  background: rgb(255, 255, 255);
+  position:absolute;
+  left:0px;
+  top:40px;
+  height: 80px;
+  width:10000px;
+  transition: 0.5s;
+}
+.positionX-track {
+  background: rgb(255, 165, 165);
+  position:absolute;
+  left:0px;
+  top:160px;
+  height: 80px;
+  width:10000px;
+}
+.width-track {
+  background: rgb(184, 223, 107);
+  position:absolute;
+  left:0px;
+  top:280px;
+  height: 80px;
+  width:10000px;
+}
+.color-track {
+  background: rgb(83, 195, 208);
+  position:absolute;
+  left:0px;
+  top:400px;
+  height: 80px;
+  width:10000px;
 }
 </style>
